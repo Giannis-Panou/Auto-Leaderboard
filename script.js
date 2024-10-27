355bb52733ab53d1c91024588730c18ad731e0d4
Giannis Panou <giannispanou500@gmail.com>
2024-10-27 17:51:15 +0200

    First attempt on Team Standings

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
 const pointsSystem = [30, 24, 21, 19, 17, 15, 13, 11, 9, 7, 5, 4, 3, 2, 1];
 const powerStagePoints = [5, 4, 3, 2, 1];
 let leaderboard = {};
+let teamLeaderboard = {};
 
-// // Parse JSON
-// function parseJsonResults(file) {
-// 	const reader = new FileReader();
-
-// 	reader.onload = function (event) {
-// 		try {
-// 			const results = JSON.parse(event.target.result);
-// 			updateLeaderboard(results); // Update leaderboard with the parsed data
-// 		} catch (error) {
-// 			console.error('Error parsing JSON:', error);
-// 			alert('Error parsing JSON file. Please check the file format.');
-// 		}
-// 	};
-
-// 	reader.readAsText(file); // Read the file content as text
-// }
-
-// Parse CSV
+// Process CSV
 function processCsvResults(file) {
 	const reader = new FileReader();
 
@@ -37,9 +21,27 @@ function processCsvResults(file) {
 	reader.readAsText(file);
 }
 
+// Process Team CSV
+function processTeamCsv(file) {
+	const reader = new FileReader();
+
+	reader.onload = function (event) {
+		try {
+			const csvResults = event.target.result;
+			const teams = csvToTeamJson(csvResults);
+			localStorage.setItem('teams', JSON.stringify(teams));
+		} catch (error) {
+			console.error('Error processing Teams CSV:', error);
+		}
+	};
+
+	reader.readAsText(file);
+}
+
 // CSV to JSON
 function csvToJson(csv) {
 	const lines = csv.split('\n');
+	const teamsFromStorage = JSON.parse(localStorage.getItem('teams'));
 
 	const results = lines
 		.slice(1)
@@ -47,9 +49,18 @@ function csvToJson(csv) {
 			const values = line.split(',');
 
 			if (values.length >= 2) {
+				const username = values[1].trim();
+				const place = parseInt(values[0].trim(), 10);
+
+				const teamEntry = teamsFromStorage.find(
+					(team) => team.username === username
+				);
+				const teamName = teamEntry ? teamEntry.team : null;
+
 				return {
-					username: values[1].trim(),
-					place: parseInt(values[0].trim(), 10),
+					username,
+					place,
+					team: teamName,
 				};
 			}
 
@@ -60,23 +71,46 @@ function csvToJson(csv) {
 	return results;
 }
 
+// CSV to Team JSON
+function csvToTeamJson(csv) {
+	const lines = csv.split('\n');
+
+	const teams = lines
+		.slice(1)
+		.map((line) => {
+			const values = line.split(',');
+			if (values.length >= 2) {
+				return {
+					username: values[1].trim(),
+					team: values[2].trim(),
+				};
+			}
+			return null;
+		})
+		.filter((result) => result !== null);
+	return teams;
+}
+
 // Update leaderboard
 function updateLeaderboard(playersResults, file) {
-	if (file.name.endsWith('_powerstage.csv')) {
-		playersResults.forEach((player) => {
-			const { username, place } = player;
-			const points = powerStagePoints[place - 1] || 0;
-			leaderboard[username] = (leaderboard[username] || 0) + points;
-		});
-	} else {
-		playersResults.forEach((player) => {
-			const { username, place } = player;
-			const points = pointsSystem[place - 1] || 0;
-			leaderboard[username] = (leaderboard[username] || 0) + points;
-		});
-	}
+	const pointsArray = file.name.endsWith('_powerstage.csv')
+		? powerStagePoints
+		: pointsSystem;
+
+	playersResults.forEach((player) => {
+		const { username, place, team } = player;
+		const points = pointsArray[place - 1] || 0;
+
+		leaderboard[username] = (leaderboard[username] || 0) + points;
+
+		if (team) {
+			teamLeaderboard[team] = (teamLeaderboard[team] || 0) + points;
+		}
+	});
+
 	saveLeaderboard();
 	displayLeaderboard();
+	displayTeamLeaderboard();
 }
 
 // Display leaderboard
@@ -88,21 +122,47 @@ function displayLeaderboard() {
 	const resultsContainer = document.querySelector('.results-container');
 	resultsContainer.innerHTML = '';
 
-	const leaderboardHtml = sortedLeaderboard
-		.map(
-			([username, points], index) =>
-				`<div class="leaderboard-entry">${
-					index + 1
-				}. ${username}: ${points} points</div>`
-		)
-		.join('');
+	if (sortedLeaderboard.length > 0) {
+		const leaderboardHtml = sortedLeaderboard
+			.map(
+				([username, points], index) =>
+					`<div class="leaderboard-entry">${
+						index + 1
+					}. ${username}: ${points} points</div>`
+			)
+			.join('');
 
-	resultsContainer.innerHTML = `<h2>Leaderboard</h2>${leaderboardHtml}`;
+		resultsContainer.innerHTML = `<h2>Leaderboard</h2>${leaderboardHtml}`;
+	}
+}
+
+// Display Team leaderboard
+function displayTeamLeaderboard() {
+	const sortedLeaderboard = Object.entries(teamLeaderboard).sort(
+		(a, b) => b[1] - a[1]
+	);
+
+	const resultsContainer = document.querySelector('.team-results-container');
+	resultsContainer.innerHTML = '';
+
+	if (sortedLeaderboard.length > 0) {
+		const leaderboardHtml = sortedLeaderboard
+			.map(
+				([team, points], index) =>
+					`<div class="leaderboard-entry">${
+						index + 1
+					}. ${team}: ${points} points</div>`
+			)
+			.join('');
+
+		resultsContainer.innerHTML = `<h2>Team Leaderboard</h2>${leaderboardHtml}`;
+	}
 }
 
 // Save leaderboard to localStorage
 function saveLeaderboard() {
 	localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
+	localStorage.setItem('teamLeaderboard', JSON.stringify(teamLeaderboard));
 }
 
 // Load leaderboard from localStorage
@@ -114,6 +174,15 @@ function loadLeaderboard() {
 	}
 }
 
+// Load Team leaderboard from localStorage
+function loadTeamLeaderboard() {
+	const savedLeaderboard = localStorage.getItem('teamLeaderboard');
+	if (savedLeaderboard) {
+		teamLeaderboard = JSON.parse(savedLeaderboard);
+		displayLeaderboard();
+	}
+}
+
 // Clear points but keep player names
 function clearPoints() {
 	Object.keys(leaderboard).forEach((username) => {
@@ -121,6 +190,25 @@ function clearPoints() {
 	});
 	saveLeaderboard(); // Save the cleared leaderboard to localStorage
 	displayLeaderboard(); // Update the displayed leaderboard
+}
+
+// Clear team points but keep team names
+function clearTeamPoints() {
+	Object.keys(teamLeaderboard).forEach((team) => {
+		teamLeaderboard[team] = 0; // Set points to 0 but keep names
+	});
+	saveLeaderboard(); // Save the cleared leaderboard to localStorage
+	displayTeamLeaderboard(); // Update the displayed leaderboard
+}
+
+// Clear LocalStorage
+function clearLocalStorage() {
+	localStorage.removeItem('leaderboard');
+	localStorage.removeItem('teamLeaderboard');
+	leaderboard = {};
+	teamLeaderboard = {};
+	displayLeaderboard();
+	displayTeamLeaderboard();
 }
 
 // Export leaderboard
@@ -142,6 +230,7 @@ function handleLoadResults() {
 
 	if (file) {
 		if (file.name.endsWith('.csv')) {
+			processTeamCsv(file);
 			processCsvResults(file);
 		} else {
 			alert('Please upload a CSV file.');
@@ -155,6 +244,7 @@ function handleLoadResults() {
 
 window.addEventListener('DOMContentLoaded', () => {
 	loadLeaderboard();
+	loadTeamLeaderboard();
 
 	document
 		.getElementById('load-results-btn')
@@ -165,6 +255,14 @@ window.addEventListener('DOMContentLoaded', () => {
 		.addEventListener('click', clearPoints);
 
 	document
+		.getElementById('clear-points-btn')
+		.addEventListener('click', clearTeamPoints);
+
+	document
 		.getElementById('export-results-btn')
 		.addEventListener('click', exportResults);
+
+	document
+		.getElementById('delete-all-btn')
+		.addEventListener('click', clearLocalStorage);
 });
